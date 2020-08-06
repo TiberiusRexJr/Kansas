ffd0fab2899a4692aa21dda63dfe7eed09e5b97c
EVA_1 <rouninlopez@gmail.com>
2020-08-06 00:18:18 -0500

    Working: none, Progress:SQlconnection

---
+++ Ldb/ldb_mvc1/Models/Db.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ldb_mvc1.Models
+{
+    public class db
+    {
+        #region Properties
+
+        #endregion
+
+        #region Costructor
+
+        #endregion
+
+        #region Fuctions
+        public void Connection()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource= "etechenterprises.database.windows.net";
+                builder.UserID = "Ravenone1";
+                builder.Password = "Supersix1!";
+                builder.InitialCatalog = "ldb";
+
+                SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                connection.Open();
+
+                StringBuilder sb = new StringBuilder();
+                
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        #endregion
+
+    }
+
+
+}
---
+++ Ldb/ldb_mvc1/Models/UserProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ldb_mvc1.Models
+{
+    public class UserProfile
+    {
+        int id { get; set; }
+        string firstName { get; set; }
+        string lastName { get; set; }
+        char sex { get; set; }
+        DateTime dob { get; set; }
+        int age { get; set; }
+        int phone { get; set; }
+        string email { get; set; }
+        int department { get; set; }
+        string userPictureUrl { get; set; }
+
+
+    }
+}
